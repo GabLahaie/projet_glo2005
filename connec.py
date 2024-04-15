1a21ac7c6b271f8644374072435bf9a08444f01e
GabLahaie <128499911+GabLahaie@users.noreply.github.com>
2024-04-15 08:36:41 -0400

    Update connec.py

--- connec.py
+++ connec.py
@@ -24,3 +24,34 @@ def db_connect():
         cursor.execute(sql, ('webmaster@python.org',))
         result = cursor.fetchone()
         print(result)
+
+
+app = Flask(__name__)
+db_connection = pymysql.connect(
+    host='localhost',
+    user='root',
+    password='Toshib@123',
+    database='flask_crud'
+)
+
+cursor = db_connection.cursor()
+
+def select_all_annonce():
+    request = "SELECT * FROM Annonces;"
+    cursor.execute(request)
+    annonces = cursor.fetchall()
+    return annonces
+
+def select_genre_annonce(genre):
+    request = "SELECT * FROM Annonces WHERE genre = %s"
+    cursor.execute(request, (genre,))
+    annonces = cursor.fetchall()
+    return annonces
+
+def recherche_annonce(terme_recherche):
+    request = "SELECT * FROM Annonces WHERE titre_annonce LIKE %s"
+    cursor.execute(request, ('%'+terme_recherche+'%',))
+    annonces = cursor.fetchall()
+
+    return annonces
+
